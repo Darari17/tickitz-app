48a522bcf895f59298762ea1c087fe95b72886e4
farid <faridramadhan59@gmail.com>
2025-07-31 00:02:41 +0700

    fix: change width component subscriber

--- src/components/Subscriber.jsx
+++ src/components/Subscriber.jsx
@@ -1,7 +1,7 @@
 export const Subscriber = () => {
   return (
-    <div className="flex flex-row justify-center items-center relative">
-      <form className="flex flex-col align-middle justify-center items-center w-[90%]  bg-[#2563eb] rounded-[20px] gap-7 py-10 ">
+    <div className="flex flex-row justify-center items-center relative mx-25">
+      <form className="flex flex-col align-middle justify-center items-center w-full bg-[#2563eb] rounded-[20px] gap-7 py-10 ">
         <div>
           <div className="text-[#ffffff] font-normal text-4xl/[50px] tracking-wider ">
             Subscribe to our newsletter
@@ -21,7 +21,7 @@ export const Subscriber = () => {
           <button className="bg-white w-56 h-14 rounded-[9px] text-[18px]/[28px] font-bold text-[#1d4ed8] cursor-pointer">
             Subscribe Now
           </button>
-          <div className="absolute border-6 border-white w-[200px] h-[200px] rounded-full right-0 bottom-0 translate-y-35 overflow-hidden"></div>
+          <div className="absolute border-6 border-white w-[200px] h-[200px] rounded-full right-0 bottom-0 translate-y-30 translate-x-20 overflow-hidden"></div>
         </div>
       </form>
     </div>
