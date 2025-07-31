521d1a3fc2564138fccbadc071a17a3388ede821
farid <faridramadhan59@gmail.com>
2025-07-31 08:53:21 +0700

    feat: add path of payment page

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,22 @@ import "./index.css";
 import { Movie } from "./pages/movie/Movie";
 import { Register } from "./pages/auth/Register";
 import { Login } from "./pages/auth/Login";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { RouterLayout } from "./RouterLayout";
+import { Payment } from "./pages/payment/Payment";
+import { Home } from "./pages/home/Home";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route element={<Register />} path="/register" />
         <Route element={<Login />} path="/login" />
-        <Route element={<Movie />} path="/movie" />
+        <Route element={<Register />} path="/register" />
+        <Route element={<RouterLayout />}>
+          <Route element={<Home />} index />
+          <Route element={<Movie />} path="/movie" />
+          <Route element={<Payment />} path="/payment" />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
